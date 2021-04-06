d41b0dd16c4dcc6de840f17cdbffe454646038d0
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-04-06 09:48:13 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -12,6 +12,59 @@ def render_main():
 def render_page1():
     return render_template('Data-For-Majors.html', firstfact = "", secondfact = "", thirdfact = "", majorsdata = get_major_options())
 
+@app.route("/response")
+def return_data():
+    major_selected = request.args['major']
+    return render_template('Data-For-Majors.html', firstfact = salaries(major_selected), secondfact = demographics(major_selected), thirdfact = education_type(major_selected), majorsdata = get_major_options())
+
+
+def salaries(selected_major):
+    with open('graduates.json') as demographics_data:
+        majors = json.load(demographics_data)
+    mean_salary = 0.0
+    average_salary = 0.0
+    standard_deviation = 0.0
+    for major in majors:
+        if major["Year"] == 2015 and major["Education"]["Major"] == selected_major:
+            mean_salary = str(major["Salaries"]["Mean"])
+            average_salary = str(major["Salaries"]["Average"])
+            standard_deviation = str(major["Salaries"]["Standard Deviation"])
+    return "The mean salary for " + selected_major + " is " + mean_salary + ". " + "The average salary is " + average_salary + ". The standard deviation of the salaries data is " + standard_deviation + "."
+
+def demographics(selected_major):
+    with open('graduates.json') as demographics_data:
+        majors = json.load(demographics_data)
+    asians = 0.0
+    whites = 0.0
+    minorities = 0.0
+    females = 0.0
+    males = 0.0
+    for major in majors:
+        if major["Year"] == 2015 and major["Education"]["Major"] == selected_major:
+            asians = str(major["Demographics"]["Ethnicity"]["Asians"])
+            whites = str(major["Demographics"]["Ethnicity"]["Whites"])
+            minorities = str(major["Demographics"]["Ethnicity"]["Minorities"])
+            females = str(major["Demographics"]["Gender"]["Females"])
+            males = str(major["Demographics"]["Gender"]["Males"])
+    return "There were " + whites + " whites with a degree in this major in 2015, " + asians + " Asians, and " + minorities + " other minorities. There are " + females + " females and " + males + " males."
+
+def education_type(selected_major):
+    with open('graduates.json') as demographics_data:
+        majors = json.load(demographics_data)
+    total = 0.0
+    bachelors = 0.0
+    doctorates = 0.0
+    masters = 0.0
+    professionals = 0.0
+    for major in majors:
+        if major["Year"] == 2015 and major["Education"]["Major"] == selected_major:
+            total = str(major["Demographics"]["Total"])
+            bachelors = str(major["Education"]["Degrees"]["Bachelors"])
+            doctorates = str(major["Education"]["Degrees"]["Doctorates"])
+            masters = str(major["Education"]["Degrees"]["Masters"])
+            professionals = str(major["Education"]["Degrees"]["Professionals"])
+    return "There were a total of " + total + " people with a degree in this major in 2015 (see FAQ). There were " + professionals + " people with professional degrees, " + bachelors + " people with a bachelor's degree, " + masters + " people with a master's degree, and " + doctorates + " people with a doctoral degree." 
+
 def get_major_options():
     with open('graduates.json') as demographics_data:
         majors = json.load(demographics_data)
