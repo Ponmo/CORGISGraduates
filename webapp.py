e8a5b3d61209cd6736ccca7314045d9d121114f8
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-04-08 09:56:01 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,16 +10,12 @@ def render_main():
 
 @app.route("/Data-For-Majors")
 def render_page1():
-    return render_template('Data-For-Majors.html', firstfact = "", secondfact = "", thirdfact = "", majorsdata = get_major_options())
+    return render_template('Data-For-Majors.html', firstfact = "", secondfact = "", thirdfact = "", majorsdata = get_major_options(), graph = "")
 
 @app.route("/response")
 def return_data():
     major_selected = request.args['major']
-    return render_template('Data-For-Majors.html', firstfact = salaries(major_selected), secondfact = demographics(major_selected), thirdfact = education_type(major_selected), majorsdata = get_major_options())
-
-@app.route("/g1")
-def render_g1():
-    return render_template('g1.html', majorsdata = get_major_options())
+    return render_template('Data-For-Majors.html', firstfact = salaries(major_selected), secondfact = demographics(major_selected), thirdfact = education_type(major_selected), majorsdata = get_major_options(), graph = Markup("<div id="chartContainer" style="height: 370px; width: 100%;"></div><script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>"))
 
 def salaries(selected_major):
     with open('graduates.json') as demographics_data:
