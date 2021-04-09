5b25fb9b5c0e5bcacc59d24f2a98cb185ee427e7
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-04-09 09:05:36 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,7 +15,7 @@ def render_page1():
 @app.route("/response")
 def return_data():
     major_selected = request.args['major']
-    return render_template('Data-For-Majors.html', firstfact = salaries(major_selected), secondfact = demographics(major_selected), thirdfact = education_type(major_selected), majorsdata = get_major_options(), graphdata = get_graph_data(), graph = Markup("<div id="chartContainer" style="height: 370px; width: 100%;"></div><script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>"))
+    return render_template('Data-For-Majors.html', firstfact = salaries(major_selected), secondfact = demographics(major_selected), thirdfact = education_type(major_selected), majorsdata = get_major_options())
 
 def salaries(selected_major):
     with open('graduates.json') as demographics_data:
@@ -62,14 +62,7 @@ def education_type(selected_major):
             doctorates = str(major["Education"]["Degrees"]["Doctorates"])
             masters = str(major["Education"]["Degrees"]["Masters"])
             professionals = str(major["Education"]["Degrees"]["Professionals"])
-    return "Education: There were a total of " + total + " people with a degree in this major in 2015 (see FAQ). There were " + professionals + " people with professional degrees, " + bachelors + " people with a bachelor's degree, " + masters + " people with a master's degree, and " + doctorates + " people with a doctoral degree." 
-
-def get_graph_data():
-    with open('graduates.json') as demographics_data:
-        majors = json.load(demographics_data)
-    
-    
-    
+    return "Education: There were a total of " + total + " people with a degree in this major in 2015 (see FAQ). There were " + professionals + " people with professional degrees, " + bachelors + " people with a bachelor's degree, " + masters + " people with a master's degree, and " + doctorates + " people with a doctoral degree."     
     
 def get_major_options():
     with open('graduates.json') as demographics_data:
